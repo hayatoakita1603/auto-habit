4b0855aeaa442ed6e54cca4640d0b64dbc1fb6f8
hayatoakita1603 <hayatoakita1603@gmail.com>
2026-03-14 09:17:45 +0000

    型エラーの解消

--- app/src/services/notificationService.ts
+++ app/src/services/notificationService.ts
@@ -32,7 +32,7 @@ export const scheduleHabitNotification = async (
 		content: {
 			title: habit.name,
 			body,
-			...(photoPath ? { attachments: [{ url: photoPath }] } : {}),
+			...(photoPath ? { attachments: [{ identifier: null, type: null, url: photoPath }] } : {}),
 		},
 		trigger: {
 			type: Notifications.SchedulableTriggerInputTypes.CALENDAR,
